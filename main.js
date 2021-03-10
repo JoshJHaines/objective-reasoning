f66a70fb5acffa2bba99144fb86c24e3ddd50173
JoshuaJHaines <joshjhaines3@gmail.com>
2021-03-10 09:55:29 -0500

    created getsHardestHomework' funciton. Returns empty str for empty arrays. Now I am trying to loop through an array to push scores into new array

--- main.js
+++ main.js
@@ -1,67 +1,72 @@
 // ┌─────────────────┐
-// │ Your code here! │	
+// │ Your code here! │
 // └─────────────────┘
-function isAdmin(obj){
-    if (obj.userRole === 'ADMIN'){
-        return true
-    } else return false
+function isAdmin(obj) {
+	if (obj.userRole === "ADMIN") {
+		return true;
+	} else return false;
 }
 
-function getEmail(obj){
-    return `${obj.firstName.toLowerCase()}.${obj.lastName.toLowerCase()}@codeimmersives.com`
+function getEmail(obj) {
+	return `${obj.firstName.toLowerCase()}.${obj.lastName.toLowerCase()}@codeimmersives.com`;
 }
 
-function getPlaylistLength(obj){
-    return obj.songs.length
+function getPlaylistLength(obj) {
+	return obj.songs.length;
 }
 
-//
+
 function getHardestHomework(obj){
-    lowScore = 100
-    hardy = obj.name //where score === lowScore
-    if (obj === undefined){
+    arrOfScores = []
+    lowestScore = Math.min(...arrOfScores)
+    lowestScore
+    if (obj[0]!== undefined){
+        for (score of obj){
+            arrOfScores.push(obj.averageScore)
+        }
+        return obj[0].name
+    } else {
         return ''
     }
-    for (averageScore in obj){
-        if (averageScore < lowScore){
-            highScore = averageScore
-        }
-    }
-    return obj.name
 }
+test = getHardestHomework([
+    { name: 'homework 1', averageScore: 50 }
+])
 
-function createPhonebook(name,phone){
-    let obj = {};
-    for (let i = 0; i < name.length; i++){
-        obj[name[i]] = phone[i];
-    }
-    return obj;
+test
+
+function createPhonebook(name, phone) {
+	let obj = {};
+	for (let i = 0; i < name.length; i++) {
+		obj[name[i]] = phone[i];
+	}
+	return obj;
 }
 
 // ┌─────────────────────────────────────┐
 // │ Do not modify code below this line. │
 // └─────────────────────────────────────┘
 
-if (typeof isAdmin === 'undefined') {
-    isAdmin = undefined;
+if (typeof isAdmin === "undefined") {
+	isAdmin = undefined;
 }
-if (typeof getEmail === 'undefined') {
-    getEmail = undefined;
+if (typeof getEmail === "undefined") {
+	getEmail = undefined;
 }
-if (typeof getPlaylistLength === 'undefined') {
-    getPlaylistLength = undefined;
+if (typeof getPlaylistLength === "undefined") {
+	getPlaylistLength = undefined;
 }
-if (typeof getHardestHomework === 'undefined') {
-    getHardestHomework = undefined;
+if (typeof getHardestHomework === "undefined") {
+	getHardestHomework = undefined;
 }
-if (typeof createPhonebook === 'undefined') {
-    createPhonebook = undefined;
+if (typeof createPhonebook === "undefined") {
+	createPhonebook = undefined;
 }
 
 module.exports = {
-    isAdmin,
-    getEmail,
-    getPlaylistLength,
-    getHardestHomework,
-    createPhonebook
+	isAdmin,
+	getEmail,
+	getPlaylistLength,
+	getHardestHomework,
+	createPhonebook,
 };
